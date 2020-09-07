7f1f0378bf0d488ee1e81351c9c68e322bef5bb0
septwood <septwood@qq.com>
2020-09-07 22:46:53 +0800

    css

--- code/gallery/index.js
+++ code/gallery/index.js
@@ -2,9 +2,12 @@ function show(pic) {
     var source = pic.getAttribute('href');
     var placeholder = document.getElementById('placeholder');
     placeholder.setAttribute('src', source);
+    var text = pic.getAttribute('title');
+    var des = document.querySelector('#description');
+    des.firstChild.nodeValue = text;
 }
 
-var as  = document.getElementsByTagName('a');
+var as = document.getElementsByTagName('a');
 for (var i = 0; i < as.length; i++) {
     as[i].addEventListener('click', function (event) {
         show(this);
